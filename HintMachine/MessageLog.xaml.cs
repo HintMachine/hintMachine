3030309e3a86083861ea30b9f3d16911e7c744b7
Dinopony <dinopony.gg@gmail.com>
2023-09-19 11:52:59 +0200

    Slightly improved MessageLog performance

--- HintMachine/MessageLog.xaml.cs
+++ HintMachine/MessageLog.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Shapes;
 
@@ -17,14 +16,26 @@ namespace HintMachine
 
         public void AddMessage(string message, LogMessageType logMessageType)
         {
-            DockPanel messagePanel = new DockPanel();
-            messagePanel.LastChildFill = true;
+            // If view was already at the bottom before adding the element, auto-scroll to prevent the user from
+            // having to scroll manually each time there are new messages
+            bool scrollToBottom = (scrollViewer.VerticalOffset == scrollViewer.ScrollableHeight);
 
+            int rowID = grid.RowDefinitions.Count;
+            RowDefinition rowDef = new RowDefinition();
+            rowDef.Height = GridLength.Auto;
+            grid.RowDefinitions.Add(rowDef);
+
+            // Add a colored decoration rectangle to quickly see message type
             Rectangle rectangle = new Rectangle();
             rectangle.VerticalAlignment = VerticalAlignment.Stretch;
             rectangle.Width = 5;
             rectangle.Fill = new SolidColorBrush(Logger.GetColorForMessageType(logMessageType));
+            Grid.SetColumn(rectangle, 0);
+            Grid.SetRow(rectangle, rowID);
+            grid.Children.Add(rectangle);
 
+            // Add a readonly TextBox containing the message text
+            // (we use a TextBox instead of a TextBlock to enable text selection) 
             TextBox textbox = new TextBox
             {
                 Text = message,
@@ -35,34 +46,20 @@ namespace HintMachine
                 BorderThickness = new Thickness(0),
                 IsReadOnly = true
             };
-
             /*
             if (panel.Children.Count % 2 == 1)
                 textbox.Background = new SolidColorBrush(Color.FromRgb(210, 210, 210));
             else */
             textbox.Background = Brushes.Transparent;
-            textbox.Background = new SolidColorBrush(Color.FromRgb(238, 238, 238));
-            
+            //textbox.Background = new SolidColorBrush(Color.FromRgb(238, 238, 238));
+
             if (logMessageType == LogMessageType.ERROR)
                 textbox.FontWeight = FontWeights.Bold;
 
-            // If view was already at the bottom before adding the element, auto-scroll to prevent the user from
-            // having to scroll manually each time there are new messages
-            bool scrollToBottom = (scrollViewer.VerticalOffset == scrollViewer.ScrollableHeight);
+            Grid.SetColumn(textbox, 1);
+            Grid.SetRow(textbox, rowID);
+            grid.Children.Add(textbox);
 
-            messagePanel.Children.Add(rectangle);
-            messagePanel.Children.Add(textbox);
-
-            messagePanel.MouseEnter += (object sender, MouseEventArgs e) => {
-                
-            };
-            messagePanel.MouseLeave += (object sender, MouseEventArgs e) => {
-                
-            };
-
-            messagePanel.Margin = new Thickness(0,0,0,1);
-            panel.Children.Add(messagePanel);
-            
             if (scrollToBottom)
                 scrollViewer.ScrollToBottom();
         }
