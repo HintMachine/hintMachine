d1098c6d2846be75663a3a09bba4095cec5b9e0c
Dinopony <dinopony.gg@gmail.com>
2023-09-25 21:25:49 +0200

    Added a checkbox to filter non-progression items from HintView

--- HintMachine/HintsView.xaml.cs
+++ HintMachine/HintsView.xaml.cs
@@ -1,16 +1,16 @@
-﻿using Archipelago.MultiClient.Net;
-using System;
-using HintMachine.Games;
-using System.Collections;
+﻿using HintMachine.Games;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
+using Archipelago.MultiClient.Net.Enums;
 
 namespace HintMachine
 {
     public partial class HintsView : UserControl
     {
+        private List<HintDetails> _fullHintsList = null;
+
         public HintsView()
         {
             InitializeComponent();
@@ -18,14 +18,24 @@ namespace HintMachine
 
         public void UpdateItems(List<HintDetails> knownHints)
         {
-            List<HintDetails> knownNotFoundHints = new List<HintDetails>();
-            foreach (HintDetails hint in knownHints)
-                if (!hint.Found)
-                    knownNotFoundHints.Add(hint);
+            _fullHintsList = knownHints;
 
+            List<HintDetails> filteredHints = new List<HintDetails>();
+            foreach (HintDetails hint in knownHints)
+            {
+                // Filter out already found items
+                if (hint.Found)
+                    continue;
+                // Filter out non-progression items if related checkbox is checked
+                if (checkboxProgression.IsChecked == true && !hint.ItemFlags.HasFlag(ItemFlags.Advancement))
+                    continue;
+
+                filteredHints.Add(hint);
+            }
+            
             Dispatcher.Invoke(() =>
             {
-                hintsList.ItemsSource = knownNotFoundHints;
+                hintsList.ItemsSource = filteredHints;
             });
         }
 
@@ -48,6 +58,12 @@ namespace HintMachine
 
             hintsList.Items.SortDescriptions.Clear();
             hintsList.Items.SortDescriptions.Add(new SortDescription(sortBy, direction));
+        }
+
+        private void OnCheckboxProgressionChecked(object sender, RoutedEventArgs e)
+        {
+            if(hintsList != null && _fullHintsList != null)
+                UpdateItems(_fullHintsList);
         }
     }
 }
