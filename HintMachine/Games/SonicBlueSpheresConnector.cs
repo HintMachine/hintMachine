8a040c85d81935107746061b49bf657549a111f1
Dinopony <dinopony.gg@gmail.com>
2023-10-09 21:06:38 +0200

    S3 Blue Spheres: Added mention of the only Bizhawk version that works

--- HintMachine/Games/SonicBlueSpheresConnector.cs
+++ HintMachine/Games/SonicBlueSpheresConnector.cs
@@ -23,7 +23,7 @@
                           "using the wondrous Lock-On Technology™, it brought you to a secret standalone version " +
                           "of the \"Get Blue Spheres\" minigame from Sonic The Hedgehog 3.\n\n" +
                           "Play an almost infinite stream of procedurally generated levels, collect all the Blue Spheres and get hints!";
-            SupportedVersions = "Emulator: Bizhawk (using default Genesis Plus GX core)\n" +
+            SupportedVersions = "Emulator: Bizhawk 2.9.1 (using default Genesis Plus GX core)\n" +
                                 "ROM: 'Sonic & Knuckles + Sonic the Hedgehog' or 'Blue Spheres Plus'";
 
             Quests.Add(_levelsQuest);
