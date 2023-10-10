d5a3b7d78b6097d2bced60143a7c54b22525720a
Dinopony <dinopony.gg@gmail.com>
2023-10-10 21:20:16 +0200

    Updated version number

--- HintMachine/Globals.cs
+++ HintMachine/Globals.cs
@@ -9,7 +9,7 @@ namespace HintMachine
     internal static class Globals
     {
         public const string ProgramName = "HintMachine";
-        public const string ProgramVersion = "1.0.2";
+        public const string ProgramVersion = "1.0.3";
 
         public const int TickInterval = 100;
         public const int HintQueueInterval = 2000;
