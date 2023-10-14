30ce9eaf4e8047813084620c320f7f6e673bcde4
Nicholas Brochu <info@nicholasbrochu.com>
2023-10-14 17:04:50 -0400

    rework luck be a landlord

--- HintMachine/Games/LuckBeALandlordConnector.cs
+++ HintMachine/Games/LuckBeALandlordConnector.cs
@@ -11,11 +11,15 @@ namespace HintMachine.Games
 
         private ProcessRamWatcher _ram = null;
 
+        private long? _bufferedSpinCount = null;
+
         public LuckBeALandlordConnector()
         {
             Name = "Luck be a Landlord";
-            Description = "Roguelike deckbuilder about using a slot machine to earn rent money and defeat capitalism";
-            SupportedVersions = "Steam";
+            Description = "Roguelike deckbuilder about using a slot machine to earn rent money and defeat capitalism. Your landlord is knocking on your door. You have one coin left to your name. You insert the coin into your slot machine...and...JACKPOT! Luck be a Landlord, tonight!";
+            Platform = "PC";
+            SupportedVersions.Add("Steam");
+            CoverFilename = "luck_be_a_landord.png";
             Author = "Serpent.AI";
 
             Quests.Add(_coinQuest);
@@ -34,16 +38,27 @@ namespace HintMachine.Games
 
         public override bool Poll()
         {
-            long coinAddress = _ram.ResolvePointerPath64(_ram.BaseAddress + 0x21EFC90, new int[] { 0x218, 0x108, 0x10, 0x58, 0x20, 0x660 });
-            long coinValue = (long)_ram.ReadDouble(coinAddress + 0x8);
+            if (_ram.TestProcess() == false) { return false; }
 
-            if (coinValue > 1)
+            long coinStructAddress = _ram.ResolvePointerPath64(_ram.BaseAddress + 0x21EFC90, new int[] { 0x218, 0x108, 0x10, 0x58, 0x20, 0x660 });
+            long spinStructAddress = _ram.ResolvePointerPath64(_ram.BaseAddress + 0x21EFC90, new int[] { 0x218, 0x108, 0x38, 0x108, 0x0, 0x58, 0x20, 0x2D0 });
+
+            if (coinStructAddress > 0 && spinStructAddress > 0)
             {
-                _coinQuest.UpdateValue(coinValue);
-            }
-            else
-            {
-                _coinQuest.ForceLastMemoryReading(coinValue);
+                try
+                {
+                    long coinValue = (long)_ram.ReadDouble(coinStructAddress + 0x8);
+                    long spinValue = _ram.ReadUint32(spinStructAddress + 0x8);
+
+                    if (_bufferedSpinCount == null) { _bufferedSpinCount = spinValue; }
+
+                    if (spinValue < (long)_bufferedSpinCount) { _coinQuest.IgnoreNextValue(); }
+
+                    _coinQuest.UpdateValue(coinValue);
+
+                    _bufferedSpinCount = spinValue;
+                }
+                catch { }
             }
 
             return true;
