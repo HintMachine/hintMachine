942063b3caa5b8760cc6f8e3e49ea7b86fc19d0c
Dinopony <dinopony.gg@gmail.com>
2023-10-14 23:14:08 +0200

    Bullets Per Minute: New game

--- HintMachine/Globals.cs
+++ HintMachine/Globals.cs
@@ -51,6 +51,7 @@ namespace HintMachine
             new PacManChampionshipEditionDXConnector(),
             new MetroidPrimePinballConnector(),
             new ColumnsConnector(),
+            new BPMConnector(),
         };
 
         public static IGameConnector FindGameFromName(string name)
